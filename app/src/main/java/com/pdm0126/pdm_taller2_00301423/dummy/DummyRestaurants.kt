df61bc96f1e72ec1a5ea996debc47307fbcf9245
Oscar Diaz <00301423@uca.edu.sv>
2026-05-17 01:28:29 -0600

    Fix: Arreglar iamgenes repetidas

--- app/src/main/java/com/pdm0126/pdm_taller2_00301423/dummy/DummyRestaurants.kt
+++ app/src/main/java/com/pdm0126/pdm_taller2_00301423/dummy/DummyRestaurants.kt
@@ -37,26 +37,26 @@ val sampleRestaurants = listOf(
         id = 2,
         name = "Smash Burger",
         description = "El hogar de las hamburguesas",
-        imageUrl = "https://images.unsplash.com/photo-1565299624946-b28f40a0ae38?q=80&w=781",
+        imageUrl = "https://www.giallozafferano.es/images/195-19518/smash-burger_1200x800.jpg",
         categories = listOf("Americana", "Hamburguesas", "Comida Rápida", "Familiar"),
         menu = listOf(
             Dish(
                 id = 1,
                 name = "Classic Burger",
                 description = "Carne de res, queso cheddar y vegetales frescos",
-                imageUrl = "https://images.unsplash.com/photo-1565299624946-b28f40a0ae38?q=80&w=781"
+                imageUrl = "https://www.giallozafferano.es/images/195-19518/smash-burger_1200x800.jpg"
             ),
             Dish(
                 id = 2,
                 name = "Bacon Double Burger",
                 description = "Doble carne con tocino y salsa especial",
-                imageUrl = "https://images.unsplash.com/photo-1565299624946-b28f40a0ae38?q=80&w=781"
+                imageUrl = "https://www.giallozafferano.es/images/195-19518/smash-burger_1200x800.jpg"
             ),
             Dish(
                 id = 3,
                 name = "Crispy Chicken Burger",
                 description = "Pollo empanizado con mayonesa chipotle",
-                imageUrl = "https://images.unsplash.com/photo-1565299624946-b28f40a0ae38?q=80&w=781"
+                imageUrl = "https://www.giallozafferano.es/images/195-19518/smash-burger_1200x800.jpg"
             )
         )
     ),
@@ -65,26 +65,26 @@ val sampleRestaurants = listOf(
         id = 3,
         name = "Pizza Palace",
         description = "El palacio de las pizzas",
-        imageUrl = "https://images.unsplash.com/photo-1565299624946-b28f40a0ae38?q=80&w=781",
+        imageUrl = "https://www.shutterstock.com/image-vector/tasty-illustration-pepperoni-pizza-mushrooms-600w-2714406037.jpg",
         categories = listOf("Italiana", "Pizza", "Variedad", "Comida rapida"),
         menu = listOf(
             Dish(
                 id = 1,
                 name = "Pizza Hawaiana",
                 description = "Jamón, piña, mozzarella y salsa de tomate",
-                imageUrl = "https://images.unsplash.com/photo-1565299624946-b28f40a0ae38?q=80&w=781"
+                imageUrl = "https://www.shutterstock.com/image-vector/tasty-illustration-pepperoni-pizza-mushrooms-600w-2714406037.jpg"
             ),
             Dish(
                 id = 2,
                 name = "Pizza Suprema",
                 description = "Carne, vegetales, pepperoni y queso mozzarella",
-                imageUrl = "https://images.unsplash.com/photo-1565299624946-b28f40a0ae38?q=80&w=781"
+                imageUrl = "https://www.shutterstock.com/image-vector/tasty-illustration-pepperoni-pizza-mushrooms-600w-2714406037.jpg"
             ),
             Dish(
                 id = 3,
                 name = "Calzone Italiano",
                 description = "Masa rellena de queso, jamón y salsa italiana",
-                imageUrl = "https://images.unsplash.com/photo-1565299624946-b28f40a0ae38?q=80&w=781"
+                imageUrl = "https://www.shutterstock.com/image-vector/tasty-illustration-pepperoni-pizza-mushrooms-600w-2714406037.jpg"
             )
         )
     ),
@@ -93,26 +93,26 @@ val sampleRestaurants = listOf(
         id = 4,
         name = "Bella Pasta",
         description = "Solo una pasta, tú y yo, piénsalo",
-        imageUrl = "https://images.unsplash.com/photo-1565299624946-b28f40a0ae38?q=80&w=781",
+        imageUrl = "https://www.recetasnestlecam.com/sites/default/files/styles/recipe_detail_desktop_new/public/srh_recipes/e60896c0281485b09a0953df66afd0d4.jpg?itok=yJ57mOvF",
         categories = listOf("Italiana", "Pasta", "Familiar", "Variedad"),
         menu = listOf(
             Dish(
                 id = 1,
                 name = "Fettuccine Alfredo",
                 description = "Pasta cremosa con queso parmesano",
-                imageUrl = "https://images.unsplash.com/photo-1565299624946-b28f40a0ae38?q=80&w=781"
+                imageUrl = "https://www.recetasnestlecam.com/sites/default/files/styles/recipe_detail_desktop_new/public/srh_recipes/e60896c0281485b09a0953df66afd0d4.jpg?itok=yJ57mOvF"
             ),
             Dish(
                 id = 2,
                 name = "Spaghetti Bolognesa",
                 description = "Pasta con salsa de carne y tomate",
-                imageUrl = "https://images.unsplash.com/photo-1565299624946-b28f40a0ae38?q=80&w=781"
+                imageUrl = "https://www.recetasnestlecam.com/sites/default/files/styles/recipe_detail_desktop_new/public/srh_recipes/e60896c0281485b09a0953df66afd0d4.jpg?itok=yJ57mOvF"
             ),
             Dish(
                 id = 3,
                 name = "Ravioli de Queso",
                 description = "Pasta rellena con queso y salsa de tomate",
-                imageUrl = "https://images.unsplash.com/photo-1565299624946-b28f40a0ae38?q=80&w=781"
+                imageUrl = "https://www.recetasnestlecam.com/sites/default/files/styles/recipe_detail_desktop_new/public/srh_recipes/e60896c0281485b09a0953df66afd0d4.jpg?itok=yJ57mOvF"
             )
         )
     ),
@@ -121,26 +121,26 @@ val sampleRestaurants = listOf(
         id = 5,
         name = "Sushi Zen",
         description = "Te garantizamos que será como comer en el cielo",
-        imageUrl = "https://images.unsplash.com/photo-1565299624946-b28f40a0ae38?q=80&w=781",
+        imageUrl = "https://glovo.dhmedia.io/image/global-menu-service/GV_ES/vendor/282318/product/11739152495/b1af4549-002a-4d77-bd1c-1fcd76ab8dc4.jpg?t=W3sicmVzaXplIjp7Im1vZGUiOiJmaXQiLCJ3aWR0aCI6MzIwLCJoZWlnaHQiOjMyMH19XQ==",
         categories = listOf("Asiática", "Sushi", "Relajado", "Familiar"),
         menu = listOf(
             Dish(
                 id = 1,
                 name = "California Roll",
                 description = "Cangrejo, aguacate y pepino",
-                imageUrl = "https://images.unsplash.com/photo-1565299624946-b28f40a0ae38?q=80&w=781"
+                imageUrl = "https://glovo.dhmedia.io/image/global-menu-service/GV_ES/vendor/282318/product/11739152495/b1af4549-002a-4d77-bd1c-1fcd76ab8dc4.jpg?t=W3sicmVzaXplIjp7Im1vZGUiOiJmaXQiLCJ3aWR0aCI6MzIwLCJoZWlnaHQiOjMyMH19XQ=="
             ),
             Dish(
                 id = 2,
                 name = "Salmon Nigiri",
                 description = "Arroz japonés con salmón fresco",
-                imageUrl = "https://images.unsplash.com/photo-1565299624946-b28f40a0ae38?q=80&w=781"
+                imageUrl = "https://glovo.dhmedia.io/image/global-menu-service/GV_ES/vendor/282318/product/11739152495/b1af4549-002a-4d77-bd1c-1fcd76ab8dc4.jpg?t=W3sicmVzaXplIjp7Im1vZGUiOiJmaXQiLCJ3aWR0aCI6MzIwLCJoZWlnaHQiOjMyMH19XQ=="
             ),
             Dish(
                 id = 3,
                 name = "Ramen Tonkotsu",
                 description = "Sopa japonesa con cerdo, huevo y fideos",
-                imageUrl = "https://images.unsplash.com/photo-1565299624946-b28f40a0ae38?q=80&w=781"
+                imageUrl = "https://glovo.dhmedia.io/image/global-menu-service/GV_ES/vendor/282318/product/11739152495/b1af4549-002a-4d77-bd1c-1fcd76ab8dc4.jpg?t=W3sicmVzaXplIjp7Im1vZGUiOiJmaXQiLCJ3aWR0aCI6MzIwLCJoZWlnaHQiOjMyMH19XQ=="
             )
         )
     ),
@@ -149,26 +149,26 @@ val sampleRestaurants = listOf(
         id = 6,
         name = "Green Bowl",
         description = "Comida fresca, saludable y llena de sabor",
-        imageUrl = "https://images.unsplash.com/photo-1565299624946-b28f40a0ae38?q=80&w=781",
+        imageUrl = "https://greenbowl.com.do/wp-content/uploads/2024/03/greenbowl-shrimp-salad.png",
         categories = listOf("Saludable", "Ensaladas", "Fit", "Variedad"),
         menu = listOf(
             Dish(
                 id = 1,
                 name = "Caesar Salad",
                 description = "Lechuga, pollo, crutones y aderezo césar",
-                imageUrl = "https://images.unsplash.com/photo-1565299624946-b28f40a0ae38?q=80&w=781"
+                imageUrl = "https://greenbowl.com.do/wp-content/uploads/2024/03/greenbowl-shrimp-salad.png"
             ),
             Dish(
                 id = 2,
                 name = "Protein Bowl",
                 description = "Arroz integral, pollo, aguacate y vegetales",
-                imageUrl = "https://images.unsplash.com/photo-1565299624946-b28f40a0ae38?q=80&w=781"
+                imageUrl = "https://greenbowl.com.do/wp-content/uploads/2024/03/greenbowl-shrimp-salad.png"
             ),
             Dish(
                 id = 3,
                 name = "Veggie Wrap",
                 description = "Tortilla con vegetales frescos y salsa ligera",
-                imageUrl = "https://images.unsplash.com/photo-1565299624946-b28f40a0ae38?q=80&w=781"
+                imageUrl = "https://greenbowl.com.do/wp-content/uploads/2024/03/greenbowl-shrimp-salad.png"
             )
         )
     ),
@@ -177,26 +177,26 @@ val sampleRestaurants = listOf(
         id = 7,
         name = "Dragon Wok",
         description = "Sabores asiáticos preparados al momento",
-        imageUrl = "https://images.unsplash.com/photo-1565299624946-b28f40a0ae38?q=80&w=781",
+        imageUrl = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRNQyuKbEKyA4cb6VOKjZ3SLumlVxf53M9EiA&s",
         categories = listOf("Asiática", "China", "Fuerte", "Familiar"),
         menu = listOf(
             Dish(
                 id = 1,
                 name = "Pollo Agridulce",
                 description = "Pollo crujiente con salsa dulce y vegetales",
-                imageUrl = "https://images.unsplash.com/photo-1565299624946-b28f40a0ae38?q=80&w=781"
+                imageUrl = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRNQyuKbEKyA4cb6VOKjZ3SLumlVxf53M9EiA&s"
             ),
             Dish(
                 id = 2,
                 name = "Chow Mein",
                 description = "Fideos salteados con vegetales y salsa oriental",
-                imageUrl = "https://images.unsplash.com/photo-1565299624946-b28f40a0ae38?q=80&w=781"
+                imageUrl = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRNQyuKbEKyA4cb6VOKjZ3SLumlVxf53M9EiA&s"
             ),
             Dish(
                 id = 3,
                 name = "Arroz Frito Especial",
                 description = "Arroz con pollo, huevo y vegetales",
-                imageUrl = "https://images.unsplash.com/photo-1565299624946-b28f40a0ae38?q=80&w=781"
+                imageUrl = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRNQyuKbEKyA4cb6VOKjZ3SLumlVxf53M9EiA&s"
             )
         )
     ),
@@ -205,26 +205,26 @@ val sampleRestaurants = listOf(
         id = 8,
         name = "Sweet Heaven",
         description = "Postres dulces para cualquier antojo",
-        imageUrl = "https://images.unsplash.com/photo-1565299624946-b28f40a0ae38?q=80&w=781",
+        imageUrl = "https://s3.ppllstatics.com/diariovasco/www/multimedia/201909/09/media/cortadas/postres-sin-azucar-kPaE-RHqLy3xgNxM4BJcU7OrSbTL-1248x770@Diario%20Vasco-k71C-RHqLy3xgNxM4BJcU7OrSbTL-1248x770@Diario%20Vasco.jpg",
         categories = listOf("Postres", "Dulces", "Gustito", "Variedad"),
         menu = listOf(
             Dish(
                 id = 1,
                 name = "Chocolate Brownie",
                 description = "Brownie caliente con chocolate derretido",
-                imageUrl = "https://images.unsplash.com/photo-1565299624946-b28f40a0ae38?q=80&w=781"
+                imageUrl = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRXFhZFp9RQjHxb2hCTBFpVW30cEUaNRbQvVA&s"
             ),
             Dish(
                 id = 2,
                 name = "Cheesecake de Fresa",
                 description = "Pastel cremoso con topping de fresa",
-                imageUrl = "https://images.unsplash.com/photo-1565299624946-b28f40a0ae38?q=80&w=781"
+                imageUrl = "https://www.seriouseats.com/thmb/VAuEFUAePPa4QXEP3dhBHA4RvJA=/1500x0/filters:no_upscale():max_bytes(150000):strip_icc()/20250904-SEA-WhiteChocolateCheesecake-LorenaMasso-HERO-206c88529afb4abbb2ca8f114b1d0f2b.jpg"
             ),
             Dish(
                 id = 3,
                 name = "Donas Glaseadas",
                 description = "Donas suaves con glaseado dulce",
-                imageUrl = "https://images.unsplash.com/photo-1565299624946-b28f40a0ae38?q=80&w=781"
+                imageUrl = "https://cdn.shopify.com/s/files/1/0173/8181/8422/files/20250701184251-donutglaze.jpg?v=1751395372&width=1600&height=900"
             )
         )
     ),
@@ -233,26 +233,26 @@ val sampleRestaurants = listOf(
         id = 9,
         name = "Taco Fiesta",
         description = "Comida mexicana rápida y llena de sabor",
-        imageUrl = "https://images.unsplash.com/photo-1565299624946-b28f40a0ae38?q=80&w=781",
+        imageUrl = "https://familiakitchen.com/wp-content/uploads/2021/01/iStock-960337396-3beef-barbacoa-tacos-e1695391119564.jpg",
         categories = listOf("Mexicana", "Comida Rápida", "Familiar", "Gustito"),
         menu = listOf(
             Dish(
                 id = 1,
                 name = "Tacos al Pastor",
                 description = "Cerdo marinado con piña, cebolla y cilantro",
-                imageUrl = "https://images.unsplash.com/photo-1565299624946-b28f40a0ae38?q=80&w=781"
+                imageUrl = "https://familiakitchen.com/wp-content/uploads/2021/01/iStock-960337396-3beef-barbacoa-tacos-e1695391119564.jpg"
             ),
             Dish(
                 id = 2,
                 name = "Burrito Supremo",
                 description = "Carne, arroz, frijoles, queso y salsa",
-                imageUrl = "https://images.unsplash.com/photo-1565299624946-b28f40a0ae38?q=80&w=781"
+                imageUrl = "https://familiakitchen.com/wp-content/uploads/2021/01/iStock-960337396-3beef-barbacoa-tacos-e1695391119564.jpg"
             ),
             Dish(
                 id = 3,
                 name = "Nachos Especiales",
                 description = "Nachos con queso, carne y jalapeños",
-                imageUrl = "https://images.unsplash.com/photo-1565299624946-b28f40a0ae38?q=80&w=781"
+                imageUrl = "https://familiakitchen.com/wp-content/uploads/2021/01/iStock-960337396-3beef-barbacoa-tacos-e1695391119564.jpg"
             )
         )
     ),
@@ -261,26 +261,26 @@ val sampleRestaurants = listOf(
         id = 10,
         name = "Café Central",
         description = "Café, bebidas y postres para relajarte",
-        imageUrl = "https://images.unsplash.com/photo-1565299624946-b28f40a0ae38?q=80&w=781",
+        imageUrl = "https://hips.hearstapps.com/hmg-prod/images/cafe-1599479980.jpg?crop=1xw:0.75xh;center,top&resize=1200:*",
         categories = listOf("Bebidas", "Postres", "Café", "Familiar"),
         menu = listOf(
             Dish(
                 id = 1,
                 name = "Cappuccino Clásico",
                 description = "Café espresso con leche",
-                imageUrl = "https://images.unsplash.com/photo-1565299624946-b28f40a0ae38?q=80&w=781"
+                imageUrl = "https://hips.hearstapps.com/hmg-prod/images/cafe-1599479980.jpg?crop=1xw:0.75xh;center,top&resize=1200:*"
             ),
             Dish(
                 id = 2,
                 name = "Croissant de Jamón",
                 description = "Croissant relleno de jamón y queso",
-                imageUrl = "https://images.unsplash.com/photo-1565299624946-b28f40a0ae38?q=80&w=781"
+                imageUrl = "https://hips.hearstapps.com/hmg-prod/images/cafe-1599479980.jpg?crop=1xw:0.75xh;center,top&resize=1200:*"
             ),
             Dish(
                 id = 3,
                 name = "Muffin de Chocolate",
                 description = "Pan dulce con chispas de chocolate",
-                imageUrl = "https://images.unsplash.com/photo-1565299624946-b28f40a0ae38?q=80&w=781"
+                imageUrl = "https://hips.hearstapps.com/hmg-prod/images/cafe-1599479980.jpg?crop=1xw:0.75xh;center,top&resize=1200:*"
             )
         )
     )
